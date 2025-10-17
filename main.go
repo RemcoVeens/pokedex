ace6ba2025b30ec991d3d214d784545e87e5c21a
RemcoVeens <remcoveens03@gmail.com>
2025-10-17 14:28:59 +0200

    end of chap 1

--- main.go
+++ main.go
@@ -7,6 +7,52 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return fmt.Errorf("could not exit")
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:")
+	fmt.Println()
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+func getCommand(input []string) (cliCommand, error) {
+	if len(input) < 1 {
+		return cliCommand{}, fmt.Errorf("no command found")
+	}
+	command := input[0]
+	for _, cmd := range commands {
+		if command == cmd.name {
+			return cmd, nil
+		}
+	}
+	return cliCommand{}, fmt.Errorf("command not found")
+}
+
 func cleanInput(text string) (output []string) {
 	for _, word := range strings.Split(text, " ") {
 		if word != "" {
@@ -23,7 +69,13 @@ func main() {
 		fmt.Print("Pokedex > ")
 		if scanner.Scan() {
 			cleaned := cleanInput(scanner.Text())
-			fmt.Printf("Your command was: %v\n", cleaned[0])
+			command, err := getCommand(cleaned)
+			if err != nil {
+				fmt.Printf("error: %v\n", err)
+				continue
+			}
+			command.callback()
+
 		}
 	}
 }
